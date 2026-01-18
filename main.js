24b89933e0f014234033140ef8e17f0b4812d6b5
GAGAN GOWDA K R <110540550+GagangowdaKR@users.noreply.github.com>
2026-01-18 19:59:25 +0530

    Update job title scroll time

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var typed = new Typed('.text', {
     strings: ["Associate Software Engineer", "Junior Java Developer", "React-Native Enthusiast *"],
     typeSpeed: 50,
     backSpeed: 50,
-    backDelay: 10000,
+    backDelay: 5000,
     loop: true
 
   });
+
